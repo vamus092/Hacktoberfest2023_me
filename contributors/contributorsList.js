758387efc878867e71bb839499b10f5b3077a2e9
SUVAM DAS <dsuvam954@gmail.com>
2024-10-05 12:43:23 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,11 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+
+  {
+id:428,
+    full name:"Suvam Das"
+    username:"https://github.com/vamus092"
+    
+  },
 ];
